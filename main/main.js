eed5079077a4b76134d930376719b2ea7744fdf2
yanxuemei <mac@macdeMacBook-Pro-3.local>
2019-05-31 00:06:12 +0800

    rewrite

--- main/main.js
+++ main/main.js
@@ -1,4 +1,27 @@
+function less2(){
+    return 6;
+}
+function over2Less8(distance){
+    if(distance<=2)return 0;
+    if(distance<=8)return 0.8*(distance-2)
+    return 0.8*(8-2)
+}
+
+function over8(distance){
+    if(distance<=8)return 0;
+    return 0.8*(1+0.5)*(distance -8)
+}
+function parking(time){
+    return 0.25*time
+
+}
+
 module.exports = function main(inputs) {
+    return Math.round(
+        less2() + over2Less8(inputs.distance) + over8(inputs.distance) + parking(inputs.time)
+    )
+
+
     // write your code here...
     return "price";
 };
